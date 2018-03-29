4e778fef5873cd4ec7d5a19eaa3d9db372c3ac58
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-03-29 07:58:58 -0400

    0.1.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jasani-serverless-lumo/lein-template "0.1.2-SNAPSHOT"
+(defproject jasani-serverless-lumo/lein-template "0.1.2"
   :description "A leiningen template to generate a serverless cljs/lumo project"
   :url "https://github.com/hiteshjasani/jasani-serverless-lumo-template"
   :license {:name "Eclipse Public License"
